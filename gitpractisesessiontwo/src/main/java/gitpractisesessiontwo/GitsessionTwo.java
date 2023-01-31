1948250fe57424997d8dcf0d48b744c1103aa1db
Shivanand.chachadi <shivu.2007@gmail.com>
2023-01-31 12:38:13 +0530

    first commit from second session

---
+++ gitpractisesessiontwo/src/main/java/gitpractisesessiontwo/GitsessionTwo.java
@@ -0,0 +1,10 @@
+package gitpractisesessiontwo;
+
+public class GitsessionTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("git practise session two ");
+	}
+
+}
